8a29727337331041d0a302cf43bf145ac6cc48b7
karineveppo <karineveppo83@gmail.com>
2022-08-29 17:23:09 -0300

    Exercício 1 feito

---
+++ exercicio1/script.js
@@ -0,0 +1,8 @@
+//let nome
+//let idade
+//console.log(nome, idade)
+
+let nome = prompt("Qual é o seu nome?")
+let idade = Number (prompt("Qual é a sua idade?"))
+console.log(`Meu nome é ${nome} e minha idade é ${idade}`)
+console.log(typeof nome, typeof idade)
